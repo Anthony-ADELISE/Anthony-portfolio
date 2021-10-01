2ef84250fe97f067f4ecaa7858b0bf5b0f13fd1c
Anthony ADELISE <anthony.adelise@gmail.com>
2021-10-01 17:57:33 +0200

    Animation avec typewritter

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import './About.css';
-import SocialContact from './SocialContact';
+import React from "react";
+import "./About.css";
+import SocialContact from "./SocialContact";
+import Typewritter from "typewriter-effect";
 
-function About() {
-    return (
-        <div className="about">
-           <div className="about-top">
-                <div className="about-info">
-                Hello tout le monde 👋🏾,  je m'appelle <br/> <span className="info-name">Anthony</span>. <br/> Je suis un développeur Web et fan de <br/> <span className="info-name">Football</span>.
-                </div>
-                <div className="about-photo">
-                    <img 
-                    alt="about pick"
-                    src={require("../assets/images/Programmer.gif").default}
-                    className="picture"/>
-                </div>
-           </div>
-           <SocialContact />
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+          <h1>
+            Hello👋🏾, Moi c'est <span className="info-name"> Anthony</span>.
+            <br />Je suis un : <span className="info-name"><Typewritter 
+            onInit={(typewriter) => {
+                typewriter
+                .typeString("Developper Web 💻.")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("Fan de football ⚽.")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("Fan de technologies 📱.")
+                .start();
+            }}
+            /></span>
+          </h1>
         </div>
-    )
+        <div className="about-photo">
+          <img
+            alt="about pick"
+            src={require("../assets/images/Programmer.gif").default}
+            className="picture"
+          />
+        </div>
+      </div>
+      <SocialContact />
+    </div>
+  );
 }
 
-export default About
+export default About;
