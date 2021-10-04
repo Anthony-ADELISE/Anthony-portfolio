aa3601785e66a8e1d3f00f10e876450a2c2895ba
Anthony ADELISE <anthony.adelise@gmail.com>
2021-10-04 19:12:52 +0200

    Logo bug corrigé

--- src/components/data/WorkData.js
+++ src/components/data/WorkData.js
@@ -20,7 +20,7 @@ export const WorkData=[
         designation:"IDE infirmier",
         dateJoining:"05/09/2017",
         dateEnd:"05/06/2018",
-        ecoleLogo:"https://lh3.googleusercontent.com/proxy/A9X0pxIiVc3asPb2nCRRAsLjm_jSmXZXxaGKIeii6le3wW-2_3HtPe9j2rUcGoIWyDcUKQT_Mu1gTzPtmcN-0TYgM5mCpwylMLBuXGHtRT2Zdg4aBArCGdygLW0UowY",
+        ecoleLogo:"https://www.reseau-chu.org/typo3temp/pics/7038ef3512.jpg",
         work:"Première année d'IDE."
     },
     {
